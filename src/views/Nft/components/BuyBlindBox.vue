97fdfe939746bf2651b8b6f1561ad6bf77ad53ed
wyc <bemyself008@163.com>
2022-05-18 17:56:42 +0800

    Function button not connected to wallet page change

--- src/views/Nft/components/BuyBlindBox.vue
+++ src/views/Nft/components/BuyBlindBox.vue
@@ -219,7 +219,7 @@ export default {
             }
           }, 1000);
         }else{
-          this.balanceStatus = false
+          this.balanceStatus = this.allLoading = false
         }
       },
       deep: true,
