61a61effded4cdf3d03d52ded3d245f2fe9a13b9
wyc <bemyself008@163.com>
2022-08-10 10:04:13 +0800

    Remove the console from the official environment

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,7 @@
 const plugins = []
-if (process.env.NODE_ENV === "production") {
-  plugins.push(["transform-remove-console", { "exclude": ["error", "warn"] }])
-}
+// if (process.env.NODE_ENV === "production") {
+//   plugins.push(["transform-remove-console", { "exclude": ["error", "warn"] }])
+// }
 module.exports = {
   presets: [
     '@vue/cli-plugin-babel/preset'
